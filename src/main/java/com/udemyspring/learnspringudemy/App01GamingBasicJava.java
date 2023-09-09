3b9a5df217f8e79cf603b1b9d0ace1cd3cfe5e5d
rikaus <rkcgramme@gmail.com>
2023-09-09 11:42:44 +0530

    Update

--- src/main/java/com/udemyspring/learnspringudemy/App01GamingBasicJava.java
+++ src/main/java/com/udemyspring/learnspringudemy/App01GamingBasicJava.java
@@ -6,7 +6,8 @@ import com.udemyspring.learnspringudemy.game.PacMan;
 public class App01GamingBasicJava {
     public static void main(String[] args) {
 
-        //var game = new SuperContra();
+        var game = new SuperContra();
+
         //var game = new MarioGame();
         var game = new PacMan();  // Creating a object
         var gameRunner =  new GameRunner(game);  //Object creation + Wiring of dependency
